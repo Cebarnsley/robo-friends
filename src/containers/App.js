02b1ed4eb879ceb2de25890ed1f450a213d2793d
Candice Barnsley <cebarnsley@gmail.com>
2019-06-09 13:00:51 +0100

    changed amplify project-config file

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
 import { setSearchField, requestRobots } from '../actions';
 
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+
+import awsconfig from '../aws-exports';
+
+
 import MainPage from '../components/MainPage';
 
 import './App.css';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
 
 
 
@@ -26,6 +37,36 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component  {
+
+	constructor(props) {
+		super(props);
+		this.handleAnalyticsClick = this.handleAnalyticsClick.bind(this);
+		this.state = {
+		  analyticsEventSent: false,
+		  resultHtml: "",
+		  eventsSent: 0
+		};
+	  }
+	
+	  handleAnalyticsClick() {
+		const { aws_project_region, aws_mobile_analytics_app_id } = awsconfig;
+	
+		  Analytics.record('AWS Amplify Tutorial Event')
+			.then( (evt) => {
+				const url = `https://${aws_project_region}.console.aws.amazon.com/pinpoint/home/?region=${aws_project_region}#/apps/${aws_mobile_analytics_app_id}/analytics/events`;
+				let result = (<div>
+				  <p>Event Submitted.</p>
+				  <p>Events sent: {this.state.eventsSent + 1}</p>
+				  <a href={url} target="_blank" rel="noopener noreferrer">View Events on the Amazon Pinpoint Console</a>
+				</div>);
+	
+				this.setState({
+					analyticsEventSent: true,
+					resultHtml: result,
+					eventsSent: this.state.eventsSent + 1
+				});
+			});
+	  }
 
 	render () {
 		 return <MainPage {...this.props} />
